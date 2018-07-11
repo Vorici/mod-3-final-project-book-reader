3daaf7a612f0de2165db766d67257b2e42538898
GregS30 <gjschmitt@live.com>
2018-07-11 09:30:52 -0400

    working mvc

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -1,5 +1,4 @@
 class BookSerializer < ActiveModel::Serializer
-  attributes :id, :title, :author, :content
-
+  attributes :id, :book_num, :image_count, :title, :authors, :year, :publisher, :isbn
   belongs_to :user
 end
